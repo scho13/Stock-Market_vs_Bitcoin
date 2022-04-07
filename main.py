4113a75d795eb7206d1d57ae299a8db29a0bfb68
Shin Cho <shincho@umich.edu>
2022-04-07 17:50:19 -0400

    demo2

--- main.py
+++ main.py
@@ -2,5 +2,19 @@ import sqlite3
 import os
 import unittest
 import json
+import requests
 
-# Names: Shin, Rebecca 
+# Team Name: Machos
+# Group Members: Shin Cho, Rebecca Mao
+
+def setUpDatabase(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    cur = conn.cursor()
+    return cur, conn
+
+def usfm_api():
+    response_API = requests.get('USFR_StmtNetCost_20110930_20210930.json')
+    data = response_API.text
+    parse_json = json.loads(data)
+    return parse_json
