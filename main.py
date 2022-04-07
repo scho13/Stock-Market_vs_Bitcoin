7d8f1f3d536080c5a0dbc5d046debf7e15fc4035
Shin Cho <shincho@umich.edu>
2022-04-07 19:05:01 -0400

    print

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ def usfm_api():
     parse_json = json.loads(data)
     return parse_json
 
+print(usfm_api())
+
 def bitcoin_api(date):
     base_url = "https://api.coinpaprika.com/v1/"
     response = requests.get(base_url + "coins/btc-bitcoin/ohlcv/historical?start=" + date)
